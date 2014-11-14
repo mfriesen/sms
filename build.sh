01f57167d7533d29b66e8d9f6c43b8b2e454d316
Mike Friesen <mfriesen@gmail.com>
2014-11-13 20:55:23 -0600

    automated building deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+
+rm -r -f $GOPATH/bin/linux_amd64
+rm -r -f $GOPATH/bin/sms*
+rm -r -f $GOPATH/bin/windows_amd64
+
 GOOS=windows GOARCH=amd64 go install
 GOOS=linux GOARCH=amd64 go install
 GOOS=darwin GOARCH=amd64 go install
